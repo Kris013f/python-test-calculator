19855546eb6c2323ce7a15ad92b2fd04e8c0dad5
Kris <43845111+Kris013f@users.noreply.github.com>
2022-06-25 22:34:16 +0200

    jenkins/run.sh, Dockerfile: python:3.6-slim -> python:3.10-slim

---
+++ jenkins/run.sh
@@ -0,0 +1,12 @@
+IMAGE_NAME="test-image"; CONTAINER_NAME="test-container"
+echo "Check current working directory: $(pwd)"; ls -al
+echo "Build docker image and run container"
+docker build -t $IMAGE_NAME .
+docker run -d --name $CONTAINER_NAME $IMAGE_NAME
+echo "Copy result.xml into Jenkins container"
+rm -rf reports; mkdir reports
+docker cp $CONTAINER_NAME:/python-test-calculator/reports/result.xml reports/
+echo "Cleanup"
+docker stop $CONTAINER_NAME
+docker rm   $CONTAINER_NAME
+docker rmi  $IMAGE_NAME
