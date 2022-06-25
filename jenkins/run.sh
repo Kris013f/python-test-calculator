e40e73ec310e0c06a5814731ba91ad169b9c7f7b
Kris <43845111+Kris013f@users.noreply.github.com>
2022-06-25 22:41:51 +0200

    jenkins/run.sh

--- jenkins/run.sh
+++ jenkins/run.sh
@@ -9,4 +9,6 @@ docker cp $CONTAINER_NAME:/python-test-calculator/reports/result.xml reports/
 echo "Cleanup"
 docker stop $CONTAINER_NAME
 docker rm   $CONTAINER_NAME
-docker rmi  $IMAGE_NAME
+#docker rmi  $IMAGE_NAME
+echo "copy result to github"
+git add reports/result.xml && git commit -m "new result.xml" && git push origin
