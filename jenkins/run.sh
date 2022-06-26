181081ca09bc14ff6d94ba8fdd9850b627569d68
Kris <43845111+Kris013f@users.noreply.github.com>
2022-06-26 12:37:26 +0200

    jenkins/run.sh

--- jenkins/run.sh
+++ jenkins/run.sh
@@ -11,4 +11,4 @@ docker stop $CONTAINER_NAME
 docker rm   $CONTAINER_NAME
 #docker rmi  $IMAGE_NAME
 echo "copy result to github"
-git add reports/result.xml && git commit -m "new result.xml" && git push origin
+#git add reports/result.xml && git commit -m "new result.xml" && git push origin
